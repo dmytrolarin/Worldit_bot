f3f5b1984423b4ae64d822b3fbc06730c666b5df
Dima <you@example.com>
2025-03-25 19:39:13 +0100

    Create file requirements.txt and create a logic bot for sending messages

--- main.py
+++ main.py
@@ -3,3 +3,14 @@
 '''
 from modules import discord, ai
 
+
+def main():
+    ''''''
+    
+    # 
+    discord.bot_client.run(token = discord.TOKEN)
+
+# 
+if __name__ == '__main__':
+    # 
+    main()
--- modules/discord.py
+++ modules/discord.py
@@ -4,3 +4,36 @@
 У цьому файлi прописана логiка взаємодiї бота з  discord сервером
 (отримання, обробка та вiдправлення повiдомлень на сервер)
 '''
+from discord import Intents, Client
+
+# 
+TOKEN = ""
+
+# 
+intents = Intents.default()
+# 
+intents.message_content = True
+# 
+bot_client = Client(intents = intents)
+
+# 
+@bot_client.event
+async def on_ready():
+    ''''''
+    #
+    name_bot = bot_client.user
+    # 
+    print(f"{name_bot} запущено")
+    
+# 
+@bot_client.event
+# 
+async def on_message(message):
+    ''''''
+    
+    #
+    if message.author != bot_client.user:
+        # 
+        content_message = message.content
+        # 
+        await message.channel.send(content_message)
