dc62eaa2da0f894d06a135cef99fbfce3e640c1b
Dima <you@example.com>
2025-03-26 19:57:02 +0100

    Implemented .env and chatgpt

--- modules/ai.py
+++ modules/ai.py
@@ -4,3 +4,26 @@
 У цьому файлі прописана логіка взаємодії бота з серверами OpenAI,
 що дозволить боту формувати відповіді на основі штучного інтелекту
 '''
+from openai import AsyncOpenAI
+from dotenv import load_dotenv
+import os
+
+# 
+load_dotenv()
+# 
+OPENAI_SECRET_KEY = os.getenv("OPENAI_SECRET_KEY")
+# 
+client_openai = AsyncOpenAI(api_key= OPENAI_SECRET_KEY)
+
+async def get_response_from_chatgpt(question):
+    ''''''
+    #
+    response = await client_openai.chat.completions.create(
+        model = "gpt-4o-mini", #
+        messages = [{
+            "role": "user", #
+            "content": question # 
+        }]
+    )
+    #
+    return response.choices[0].message.content
--- modules/discord.py
+++ modules/discord.py
@@ -5,35 +5,43 @@
 (отримання, обробка та вiдправлення повiдомлень на сервер)
 '''
 from discord import Intents, Client
+from .ai import get_response_from_chatgpt 
+from dotenv import load_dotenv
+import os
 
 # 
-TOKEN = ""
+load_dotenv()
 
 # 
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+# Створюємо об'єкт, що надає боту базові права (напр., отримувати повідомлення без контенту, відслідковувати нових учасників сервера тощо)
 intents = Intents.default()
-# 
+# Надаємо боту право на читання вмісту повідомлень
 intents.message_content = True
-# 
+# Створюємо об'єкт бота з попередньо вказаними правами
 bot_client = Client(intents = intents)
 
-# 
+# Декоратор, що робить функцію нижче подією бота
 @bot_client.event
 async def on_ready():
-    ''''''
-    #
+    '''Фнгукція-подія, що відпрацює, коли бот буде запщено (бот стане онлайн)'''
+
+    # Отримуємо ім'я бота
     name_bot = bot_client.user
-    # 
+    # Виводимо інформацію у консоль, що бот з відповідним іменем запущено
     print(f"{name_bot} запущено")
     
-# 
+# Декоратор, що робить функцію нижче подією бота
 @bot_client.event
-# 
 async def on_message(message):
-    ''''''
+    '''Функція-подія, що відпрацює, коли бот отримає повідомлення від користувача'''
     
-    #
+    # Перевірка, чи не є автором отриманого повідомлення сам бот
     if message.author != bot_client.user:
         # 
-        content_message = message.content
-        # 
-        await message.channel.send(content_message)
+        response = await get_response_from_chatgpt(message.content)
+        # Отримуємо поточний канал, де повідомлення було надсілано
+        current_channel = message.channel
+        # Бот насдилає повідомлення у поточний канал
+        await current_channel.send(response)
